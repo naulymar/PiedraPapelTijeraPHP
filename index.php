ca8814f6382ef570ff5079d9b00bfb49e09cf391
naulymar <naulymar@gmail.com>
2022-10-17 23:15:03 -0400

    Add header icon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="icon" type="image/png" sizes="32x32" href="./img/piedra-papel-tijeras.png">
     <link rel="stylesheet" href="./style.css">
     <title>Piedra, Papel y Tijera</title>
 </head>
